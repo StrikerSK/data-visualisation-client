d85950a8953f921f4d391c840e4bf0391ffd54c4
StrikerSK <kristianhanus@gmail.com>
2019-12-07 15:52:16 +0100

    Recharts - PieChartYearly update

--- src/components/recharts/PieChartYearly.js
+++ src/components/recharts/PieChartYearly.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-// import "../../stylesheet/PieChart.scss";
+import "../../stylesheet/PieChart.scss";
 import PieChartElement from "./PieChartElement";
 import {pieDataGetter} from "../../lib/DataFetcher";
 import styled from "styled-components";
@@ -19,6 +19,74 @@ const PieChartContainer = styled.div`
 	grid-template-rows: 150px 1fr;
 `;
 
+const TableLayout = styled.table`
+	margin: 1em 0;
+	min-width: 300px;
+	background: #34495E;
+	color: #fff;
+	border-radius: .4em;
+	overflow: hidden;
+
+	tr {
+		border-top: 1px solid #ddd;
+		border-bottom: 1px solid #ddd;
+		border-color: #46637f;
+	}
+
+	th {
+		display: none;
+	}
+
+	td {
+		display: block;
+	}
+
+	td:first-child {
+		padding-top: .5em;
+	}
+
+	td:last-child {
+		padding-bottom: .5em;
+	}
+
+	td:before {
+		content: attr(data-th) ": ";
+		font-weight: bold;
+		width: 6.5em;
+		display: inline-block;
+	}
+
+	th td {
+		text-align: left;
+		margin: .5em 1em;
+	}
+
+	th, td:before {
+		color: #dd5;
+	}
+
+	@media (min-width: 480px) {
+		td:before {
+			display: none;
+		}
+
+		th, td {
+			display: table-cell;
+			padding: .25em .5em;
+			padding: 1em !important;
+		}
+
+		th:first-child, td:first-child {
+			padding-left: 0;
+		}
+
+		th:last-child, td:last-child {
+			padding-right: 0;
+		}
+	}
+
+`;
+
 const PieChartYearly = () => {
 	const [data, setData] = useState([]);
 	const [sum, setSum] = useState(0);
@@ -32,6 +100,7 @@ const PieChartYearly = () => {
 	};
 
 	const processData = (result) => {
+		console.log(result);
 		getSum(result);
 		setData(result);
 	};
@@ -44,14 +113,14 @@ const PieChartYearly = () => {
 		<PieChartContainer>
 			<h1>Celkový predaj lístkov PID za rok 2017</h1>
 			<DataTableLayout>
-				<table className={"rwd-table"}>
+				<TableLayout>
 					<tr>
 						<th>Kategorie</th>
 						<th>Počet</th>
 					</tr>
-					{data.map(({nazov, value}) =>
+					{data.map(({name, value}) =>
 						<tr>
-							<td>{nazov}</td>
+							<td>{name}</td>
 							<td>{value}</td>
 						</tr>
 					)}
@@ -59,9 +128,9 @@ const PieChartYearly = () => {
 						<td>Spolu</td>
 						<td>{sum}</td>
 					</tr>
-				</table>
+				</TableLayout>
 			</DataTableLayout>
-			<PieChartElement myData={pieDataGetter([], processData)} radius={250}/>
+			<PieChartElement myData={data} radius={250}/>
 		</PieChartContainer>
 	);
 };

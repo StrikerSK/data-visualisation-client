0e8089c401d3ce9f58cfb8d931ed70fa9c9c9f22
StrikerSK <kristianhanus@gmail.com>
2019-12-15 14:31:37 +0100

    Updated Checkbox for person types

--- src/components/checkboxes/CheckboxPerson.js
+++ src/components/checkboxes/CheckboxPerson.js
@@ -1,11 +1,6 @@
-import React, {useState} from "react";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import FormLabel from "@material-ui/core/FormLabel";
-import FormGroup from "@material-ui/core/FormGroup";
-import FormControl from "@material-ui/core/FormControl";
-import {useDispatch} from "react-redux";
-import {actions} from "../../lib/actions";
+import React from "react";
+import CheckboxTemplate from "./CheckboxTemplate";
+import {updatePersons} from "../../lib/actions";
 
 export const adults = "Dospelý";
 export const juniors = "Juniori";
@@ -18,98 +13,15 @@ export const children = "Deti";
 export const dataKeys = [adults, seniors, juniors, students, portableData, children];
 export const defaultPersonRequestParams = "person=Dospelý&person=Dôchodcovia&person=Študenti&person=Prenosná&person=Juniori&person=Deti";
 
+const persons = [
+	{itemName: adults, isChecked: true},
+	{itemName: juniors, isChecked: true},
+	{itemName: seniors, isChecked: true},
+	{itemName: students, isChecked: true},
+	{itemName: portable, isChecked: true},
+	{itemName: children, isChecked: true},
+];
 const CheckboxPerson = () => {
-	const dispatch = useDispatch();
-
-	const [personFilter, setPersonFilter] = useState({
-		adults: true,
-		juniors: true,
-		seniors: true,
-		portable: true,
-		students: true,
-		children: true
-	});
-
-	const checkboxHandler = ({target}) => {
-		const {name} = target;
-		const inputFilter = {...personFilter, [name]: target.checked};
-
-		setPersonFilter(inputFilter);
-		setRequestedPersons(inputFilter);
-	};
-
-	const setRequestedPersons = (inputObject) => {
-		const outputArray = [];
-
-		if (inputObject.adults) {
-			addWithPrefix(adults);
-		}
-
-		if (inputObject.seniors) {
-			addWithPrefix(seniors);
-		}
-
-		if (inputObject.students) {
-			addWithPrefix(students);
-		}
-
-		if (inputObject.portable) {
-			addWithPrefix(portable);
-		}
-
-		if (inputObject.juniors) {
-			addWithPrefix(juniors);
-		}
-
-		if (inputObject.children) {
-			addWithPrefix(children);
-		}
-
-		dispatch(actions(outputArray.join("&")));
-
-		function addWithPrefix(person) {
-			outputArray.push("person=" + person);
-		}
-	};
-
-	return (
-		<div>
-			<FormControl component={"fieldset"}>
-				<FormLabel component="legend">Filter podľa osôb</FormLabel>
-				<FormGroup row>
-					<FormControlLabel
-						control={<Checkbox name={"adults"} checked={personFilter.adults}
-						                   onChange={event => checkboxHandler(event)}/>}
-						label={adults}
-					/>
-					<FormControlLabel
-						control={<Checkbox name={"juniors"} checked={personFilter.juniors}
-						                   onChange={event => checkboxHandler(event)}/>}
-						label={juniors}
-					/>
-					<FormControlLabel
-						control={<Checkbox name={"seniors"} checked={personFilter.seniors}
-						                   onChange={event => checkboxHandler(event)}/>}
-						label={seniors}
-					/>
-					<FormControlLabel
-						control={<Checkbox name={"portable"} checked={personFilter.portable}
-						                   onChange={event => checkboxHandler(event)}/>}
-						label={portable}
-					/>
-					<FormControlLabel
-						control={<Checkbox name={"students"} checked={personFilter.students}
-						                   onChange={event => checkboxHandler(event)}/>}
-						label={students}
-					/>
-					<FormControlLabel
-						control={<Checkbox name={"children"} checked={personFilter.children}
-						                   onChange={event => checkboxHandler(event)}/>}
-						label={children}
-					/>
-				</FormGroup>
-			</FormControl>
-		</div>
-	);
+	return <CheckboxTemplate checkItems={persons} dispatchFunction={updatePersons} context={"person"}/>
 };
 export default CheckboxPerson;
--- src/lib/actions.js
+++ src/lib/actions.js
@@ -1,4 +1,4 @@
-export const actions = (personArray) => {
+export const updatePersons = (personArray) => {
 	return {
 		type: 'UPDATE_PERSON',
 		payload: personArray

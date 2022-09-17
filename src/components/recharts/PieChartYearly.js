8ac2d16441af116658faa6a21c7940f1870f8881
Teh|Striker <kristianhanus@gmail.com>
2022-09-17 22:45:06 +0200

    Code changes - Rechars pie graph

--- src/components/recharts/PieChartYearly.js
+++ src/components/recharts/PieChartYearly.js
@@ -8,58 +8,61 @@ import {generateColor} from "../../lib/Functions";
 import {accessAll} from "../../lib/ReduceAccessor";
 
 const PieChartYearly = ({months, person, validity, sellType}) => {
-	const [data, setData] = useState([]);
-	const [color, setColor] = useState("");
-	const [isLoaded, changeLoadedState] = useState(false);
+    const [data, setData] = useState([]);
+    const [color, setColor] = useState("");
+    const [isLoaded, changeLoadedState] = useState(false);
 
-	const processData = (result) => {
-		setData(result);
-		changeLoadedState(true);
-	};
+    const processData = (result) => {
+        setData(result);
+        changeLoadedState(true);
+    };
 
-	useEffect(() => {
-		setColor(generateColor());
-		pieDataGetter([months, person, validity, sellType], processData);
-	}, [months, person, validity, sellType]);
+    useEffect(() => {
+        setColor(generateColor());
+        pieDataGetter([months, person, validity, sellType], processData);
+    }, [months, person, validity, sellType]);
 
-	const pieChart = (
-		<ResponsiveContainer width="100%" height="100%">
-			<PieChart>
-				<Pie data={data}
-				     dataKey="value"
-				     nameKey="label"
-				     cx="50%"
-				     cy="50%"
-					// outerRadius={radius}
-					 fill={color}
-					 label={({cx, cy, midAngle, innerRadius, outerRadius, value, index}) => {
-						 const RADIAN = Math.PI / 180;
-						 // eslint-disable-next-line
-						 const radius = 25 + innerRadius + (outerRadius - innerRadius);
-						 // eslint-disable-next-line
-						 const x = cx + radius * Math.cos(-midAngle * RADIAN);
-						 // eslint-disable-next-line
-						 const y = cy + radius * Math.sin(-midAngle * RADIAN);
+    const pieChart = (
+        <div className="recharts-pie-graph">
+            <ResponsiveContainer width="100%" height="100%">
+                <PieChart>
+                    <Pie
+                        data={data}
+                        dataKey="value"
+                        nameKey="label"
+                        cx="50%"
+                        cy="50%"
+                        // outerRadius={radius}
+                        fill={color}
+                        label={({cx, cy, midAngle, innerRadius, outerRadius, value, index}) => {
+                            const RADIAN = Math.PI / 180;
+                            // eslint-disable-next-line
+                            const radius = 25 + innerRadius + (outerRadius - innerRadius);
+                            // eslint-disable-next-line
+                            const x = cx + radius * Math.cos(-midAngle * RADIAN);
+                            // eslint-disable-next-line
+                            const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
-						 return (
-							 <text
-								 x={x}
-								 y={y}
-								 fill={color}
-								 textAnchor={x > cx ? "start" : "end"}
-								 dominantBaseline="central"
-							 >
-								 {data[index].id} ({value})
-							 </text>
-						 );
-					 }}/>
-			</PieChart>
-		</ResponsiveContainer>
-	);
+                            return (
+                                <text
+                                    x={x}
+                                    y={y}
+                                    fill={color}
+                                    textAnchor={x > cx ? "start" : "end"}
+                                    dominantBaseline="central"
+                                >
+                                    {data[index].id} ({value})
+                                </text>
+                            );
+                        }}/>
+                </PieChart>
+            </ResponsiveContainer>
+        </div>
+    );
 
-	return (
-		<SpinnerComponent children={pieChart} isDataLoaded={isLoaded}/>
-	);
+    return (
+        <SpinnerComponent children={pieChart} isDataLoaded={isLoaded}/>
+    );
 };
 
 export default connect(accessAll)(PieChartYearly);

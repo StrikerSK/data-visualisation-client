e7664fa292648373dd4cfe68cf3c5bbdbc6a0690
Teh|Striker <kristianhanus@gmail.com>
2022-09-17 15:58:57 +0200

    Restyle components - Graph body

--- src/components/DashboardTemplate.js
+++ src/components/DashboardTemplate.js
@@ -1,10 +1,17 @@
 import React from "react";
-import DashboardHeader from "./headers/DashboardHeader";
+import ModalWindow from "./ModalWindow";
+import {LinkMenuBox} from "./LinkMenuBox";
+import GeneralHeader from "./layout/GeneralHeader";
 
 export default ({children}) => {
     return (
         <div className="dashboard-component">
-            <DashboardHeader/>
+            <GeneralHeader>
+                <ModalWindow/>
+                <h1>Predajnosť lístkov PID</h1>
+                <LinkMenuBox/>
+            </GeneralHeader>
+
             <div className="dashboard-container">
                 <div className="dashboard-element">
                     {children[0]}

417b580f58d8a432637174faf5389888483c3a57
Teh|Striker <kristianhanus@gmail.com>
2024-02-26 22:36:51 +0100

    Abstract Nivo configurations

--- src/container/nivo/NivoPieContainer.js
+++ src/container/nivo/NivoPieContainer.js
@@ -1,19 +1,7 @@
 import React from "react";
 
 import NivoPieComponent from "../../components/nivo/NivoPieComponent";
-import SelectColorScheme from "../../components/controlls/selects/SelectColorScheme";
-import CheckboxValidity from "../../components/controlls/checkboxes/CheckboxValidity";
-import CheckboxMonths from "../../components/controlls/checkboxes/CheckboxMonths";
-import CheckboxSellType from "../../components/controlls/checkboxes/CheckboxSellType";
-import CheckboxPerson from "../../components/controlls/checkboxes/CheckboxPerson";
 import GraphComponent from "../../components/layout/GraphComponent";
+import {DefaultConfiguration} from "./AbstractNivo";
 
-const configurations = [
-	<SelectColorScheme/>,
-	<CheckboxPerson/>,
-	<CheckboxValidity/>,
-	<CheckboxMonths/>,
-	<CheckboxSellType/>,
-]
-
-export default () => <GraphComponent graph={<NivoPieComponent/>} configs={configurations}/>;
+export default () => <GraphComponent graph={<NivoPieComponent/>} configs={DefaultConfiguration}/>;
--- src/container/nivo/NivoStreamContainer.js
+++ src/container/nivo/NivoStreamContainer.js
@@ -1,18 +1,6 @@
 import React from "react";
 import GraphComponent from "../../components/layout/GraphComponent";
 import NivoStreamComponent from "../../components/nivo/NivoStreamComponent";
-import CheckboxPerson from "../../components/controlls/checkboxes/CheckboxPerson";
-import CheckboxValidity from "../../components/controlls/checkboxes/CheckboxValidity";
-import CheckboxMonths from "../../components/controlls/checkboxes/CheckboxMonths";
-import CheckboxSellType from "../../components/controlls/checkboxes/CheckboxSellType";
-import SelectColorScheme from "../../components/controlls/selects/SelectColorScheme";
+import {DefaultConfiguration} from "./AbstractNivo";
 
-const configurations = [
-    <SelectColorScheme/>,
-    <CheckboxPerson/>,
-    <CheckboxValidity/>,
-    <CheckboxMonths/>,
-    <CheckboxSellType/>,
-]
-
-export default () => <GraphComponent graph={<NivoStreamComponent/>} configs={configurations}/>;
+export default () => <GraphComponent graph={<NivoStreamComponent/>} configs={DefaultConfiguration}/>;

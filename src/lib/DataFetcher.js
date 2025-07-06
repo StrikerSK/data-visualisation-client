b347325dd0c2b1cf5f0533eaae9ce7beb0f8e5f0
Teh|Striker <kristianhanus@gmail.com>
2025-07-06 15:52:26 +0200

    Axios - Creat first function

--- src/lib/DataFetcher.js
+++ src/lib/DataFetcher.js
@@ -1,17 +1,40 @@
 import axios from "axios"
+import qs from "qs"
 
-const hostUrl = "http://localhost:8080";
+const hostUrl = "http://localhost:8081";
 //const hostUrl = "https://pid-data-server.herokuapp.com/";
 
 const generateParamsQuery = (parametersList) => {
 	return parametersList.join("&");
 };
 
-export const nivoBarPath = "/nivo/coupon/bar?upperGroup=month&lowerGroup=person&"
-export const nivoPiePath = "/nivo/coupon/pie?group=year&"
+export const nivoBarPath = hostUrl + "/nivo/coupon/bar"
+export const nivoPiePath = hostUrl + "/nivo/coupon/pie"
 
 export const fetchBarData = (url, parameters) => {
-	return axios.get(hostUrl + url + generateParamsQuery(parameters));
+	//return axios.get(hostUrl + url + generateParamsQuery(parameters));
+	return axios.get(url, {
+		params: {
+			upperGroup: "month",
+			lowerGroup: "person"
+		},
+		paramsSerializer: params => {
+			return qs.stringify(params)
+		}
+	})
+};
+
+export const fetchBarDataV2 = (url, parameters) => {
+	return axios.get(url, {
+		params: {
+			upperGroup: "month",
+			lowerGroup: "validity",
+			...parameters
+		},
+		paramsSerializer: function (params) {
+			return qs.stringify(params, { arrayFormat: "repeat" });
+		},
+	})
 };
 
 export const lineDataGetter = (parameters, callbackFunction) => {
@@ -35,6 +58,6 @@ export const rechartsBarDataGetter = (parameters, callbackFunction) => {
 };
 
 export const apexDataFetcher = (parameters, callbackFunction) => {
-	axios.get(hostUrl + "/apex/data?" + generateParamsQuery(parameters))
+	axios.get(hostUrl + "/apex/coupon?upperGroup=year&lowerGroup=month&" + generateParamsQuery(parameters))
 		.then(({data}) => callbackFunction(data));
 };

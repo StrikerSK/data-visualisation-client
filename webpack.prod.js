a84aa57e977139223600e59a6c6333e591416538
Teh|Striker <kristianhanus@gmail.com>
2026-02-20 23:42:14 +0100

    Initialize GEMINI.md and modernize Webpack configuration
    - Created GEMINI.md with project overview and development instructions.
    - Modularized Webpack configuration into common, dev, and prod files.
    - Implemented production optimizations (cache busting, code splitting).
    - Switched to style-loader in dev for faster HMR.
    - Removed unused dependencies (react-scripts, html-loader) and cleaned up package.json.
    - Updated .gitignore to exclude generated CSS files.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,57 @@
 const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js')
+const common = require('./webpack.common.js');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
-    devtool: 'source-map'
+    devtool: 'source-map',
+    output: {
+        filename: '[name].[contenthash].js',
+        chunkFilename: '[name].[contenthash].chunk.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s?css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: false
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: false,
+                            api: "modern",
+                            sassOptions: {
+                                silenceDeprecations: ['legacy-js-api'],
+                            },
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+            chunkFilename: '[id].[contenthash].css',
+        })
+    ],
+    optimization: {
+        moduleIds: 'deterministic',
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                },
+            },
+        },
+    }
 });
